93d502229a0ca8026a7d6e96bf71599abf6c785b
Ferretsroq <Ferretsroq@gmail.com>
2026-01-14 12:56:46 -0500

    Fixing spacing

--- adversaries.js
+++ adversaries.js
@@ -10,7 +10,9 @@ for(let adversaryIndex = 0; adversaryIndex < adversaryData.length; adversaryInde
     const adversary = adversaryData[adversaryIndex];
     console.log(adversary.name["en-US"]);
     const ratio = 7/12;
-    const scaling = 1000;
+    const ppi = 300;
+    const heightPixels = 4.75 * ppi;
+    const scaling = heightPixels/12;
     const width = scaling * 7;
     const height = scaling * 12;
     const canvas = createCanvas(width, height);
@@ -22,16 +24,18 @@ for(let adversaryIndex = 0; adversaryIndex < adversaryData.length; adversaryInde
     context.fillStyle = 'black';
 
     TopText(adversary, context, scaling, width);
-    let [spacing, fontDenominator] = DetermineFeatFontSize(adversary, context, scaling, width);
-
+    let [spacing, fontDenominator] = DetermineFeatFontSize(adversary, context, scaling, width, heightPixels);
+    console.log(`FONT SIZE ${scaling/fontDenominator}`);
     let lineIndex = 0;
     for(let featIndex = 0; featIndex < adversary.features.length; featIndex++)
     {
         let wordIndex = 0;
-        
-        context.font = `bold ${scaling/fontDenominator}px serif`;
-        context.fillText(`${adversary.features[featIndex].name["en-US"]}`, scaling*0.015, scaling*((5+featIndex + ((spacing)*lineIndex))));
-        context.font = `${scaling/fontDenominator}px serif`;
+        lineIndex++;
+        //let lineIndex = 0;
+
+        context.font = `bold ${scaling/fontDenominator}px sans`;
+        context.fillText(`${adversary.features[featIndex].name["en-US"]}`, scaling*0.015, scaling*((5 + ((spacing)*(lineIndex)))));
+        context.font = `${scaling/fontDenominator}px sans`;
         let words = adversary.features[featIndex].description[0].paragraph["en-US"].replaceAll("<bold>", "").replaceAll("</bold>", "").replaceAll("<italic>", "").replaceAll("</italic>", "").split(" ");
         if(adversary.features[featIndex].description.length > 1 && "list" in adversary.features[featIndex].description[1])
         {
@@ -57,9 +61,10 @@ for(let adversaryIndex = 0; adversaryIndex < adversaryData.length; adversaryInde
                 }
                 wordIndex++;
             }
-            context.fillText(lineText, scaling*0.015, scaling*((5+featIndex)+(spacing) + ((spacing)*lineIndex)));
+            context.fillText(lineText, scaling*0.015, scaling*((5)+(spacing) + ((spacing)*(lineIndex))));
             lineIndex++;
         }
+        
 
     }
 
@@ -70,36 +75,36 @@ for(let adversaryIndex = 0; adversaryIndex < adversaryData.length; adversaryInde
 function TopText(adversary, context, scaling, width)
 {
     let titleDenominator = 1;
-    context.font = `${scaling/titleDenominator}px serif`;
+    context.font = `${scaling/titleDenominator}px sans`;
     while(context.measureText(adversary.name["en-US"]).width > width)
     {
         titleDenominator += 0.01;
-        context.font = `${scaling/titleDenominator}px serif`;
+        context.font = `${scaling/titleDenominator}px sans`;
     }
     context.fillText(adversary.name["en-US"], scaling*0.015, scaling);
     context.fillRect(0, scaling*1.015, width, scaling*0.015);
-    context.font = `${scaling/2}px serif`;
+    context.font = `${scaling/2}px sans`;
     context.fillText(`Tier ${adversary.tier} ${adversary.type}`, scaling*0.015, scaling*1.515);
     context.fillRect(0, scaling*1.53, width, scaling*0.015);
     let denominator = 4;
-    context.font = `${scaling/denominator}px serif`;
+    context.font = `${scaling/denominator}px sans`;
     while(context.measureText(`Motives & Tactics: ${adversary.motivesAndTactics["en-US"]}`).width > width)
     {
         denominator += 0.05
-        context.font = `${scaling/denominator}px serif`;
+        context.font = `${scaling/denominator}px sans`;
     }
     //context.font = `${scaling/denominator}px serif`;
     context.fillText(`Motives & Tactics: ${adversary.motivesAndTactics["en-US"]}`, scaling*0.015, scaling*2.015);
     
     denominator = 4;
-    context.font = `italic ${scaling/denominator}px serif`;
+    context.font = `italic ${scaling/denominator}px sans`;
     while(context.measureText(adversary.description[0].paragraph["en-US"]).width > width)
     {
         denominator += 0.05;
-        context.font = `italic ${scaling/denominator}px serif`;
+        context.font = `italic ${scaling/denominator}px sans`;
     }
     context.fillText(adversary.description[0].paragraph["en-US"], scaling*0.015, scaling*2.265);
-    context.font = `${scaling/4}px serif`;
+    context.font = `${scaling/4}px sans`;
     context.fillText(`Difficulty ${adversary.difficulty} | Thresholds ${adversary.damageThresholds.major}/${adversary.damageThresholds.severe} | HP ${adversary.hitPoints} | Stress ${adversary.stress}`, scaling*0.015, scaling*3);
     //context.fillText(`Attack ${adversary.standardAttack.name["en-US"]} +${adversary.attackModifier} ${adversary.standardAttack.range} | ${adversary.standardAttack.damage.dice[0].quantity}${adversary.standardAttack.damage.dice[0].die}+${adversary.standardAttack.damage.modifier} ${adversary.standardAttack.damage.type}`, scaling*0.015, scaling*3.5)
     context.fillText(FormatAttack(adversary), scaling*0.015, scaling*3.5)
@@ -114,24 +119,31 @@ function TopText(adversary, context, scaling, width)
     }
 }
 
-function DetermineFeatFontSize(adversary, context, scaling, width)
+function DetermineFeatFontSize(adversary, context, scaling, width, heightPixels)
 {
-    let lineIndex = 0;
     let pixelHeight = 10000;
     let spacing = 0.5;
-    let fontDenominator = 4;
-    while(pixelHeight > 7000)
+    let fontDenominator = 1;
+    const textToMeasure = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    context.font = `${scaling/fontDenominator}px sans`;
+    let lineHeight = context.measureText(textToMeasure).actualBoundingBoxAscent + context.measureText(textToMeasure).actualBoundingBoxDescent;
+    while(pixelHeight > (heightPixels*7/12) - lineHeight)
     {
         pixelHeight = 0;
         fontDenominator += 0.05;
         spacing =  1/fontDenominator;
+        context.font = `${scaling/fontDenominator}px sans`;
+        lineHeight = context.measureText(textToMeasure).actualBoundingBoxAscent + context.measureText(textToMeasure).actualBoundingBoxDescent;
+        let lineIndex = 0;
 
         for(let featIndex = 0; featIndex < adversary.features.length; featIndex++)
         {
             let wordIndex = 0;
-            context.font = `bold ${scaling/fontDenominator}px serif`;
-            pixelHeight += (scaling/fontDenominator);
-            context.font = `${scaling/fontDenominator}px serif`;
+            lineIndex++;
+            context.font = `bold ${scaling/fontDenominator}px sans`;
+            //pixelHeight += (scaling/fontDenominator)*0.8;
+            pixelHeight += lineHeight;
+            context.font = `${scaling/fontDenominator}px sans`;
             let words = adversary.features[featIndex].description[0].paragraph["en-US"].replaceAll("<bold>", "").replaceAll("</bold>", "").replaceAll("<italic>", "").replaceAll("</italic>", "").split(" ");
             if(adversary.features[featIndex].description.length > 1 && "list" in adversary.features[featIndex].description[1])
             {
@@ -143,6 +155,13 @@ function DetermineFeatFontSize(adversary, context, scaling, width)
             }
             while(wordIndex < words.length)
             {
+                if(context.measureText(words[wordIndex]).width > width*0.985)
+                {
+                    fontDenominator += 0.05;
+                    spacing = 1/fontDenominator;
+                    context.font = `${scaling/fontDenominator}px sans`;
+                    lineHeight = context.measureText(textToMeasure).actualBoundingBoxAscent + context.measureText(textToMeasure).actualBoundingBoxDescent;
+                }
                 let lineText = "";
                 while(context.measureText(lineText + words[wordIndex]).width < width*0.985 && wordIndex < words.length)
                 {
@@ -153,10 +172,15 @@ function DetermineFeatFontSize(adversary, context, scaling, width)
                     lineText += words[wordIndex] + " ";
                     wordIndex++;
                 }
-                pixelHeight += (scaling/fontDenominator) + (spacing*scaling)*2;
+                //pixelHeight += 0.8*(2*scaling/fontDenominator) + (spacing*scaling)*2;
+                pixelHeight += lineHeight;
                 lineIndex++;
             }
+            pixelHeight += lineHeight;
+            //pixelHeight += (2*scaling/fontDenominator) + (spacing*scaling)*2;
+
         }
+        console.log(pixelHeight);
     }
     
     return [spacing, fontDenominator];
